7c727c1bed76de87eba844164535962a40f3f3b0
Jonathan Emanuel <jonathan.tolentino@clicksoft.com.br>
2022-12-08 23:10:46 -0300

    change - update db config to deploy in railway

--- utils/mysql_functions.js
+++ utils/mysql_functions.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql2');
 
 var connection = mysql.createConnection({
-	host : 'containers-us-west-120.railway.app',
-	user : 'postgres',
-	password : 'HaqyWZc64UluvjkArckk',
-	database : 'railway',
-	port : 6319
+	host : process.env.PGHOST,
+	user : process.env.PGUSER,
+	password : process.env.PGPASSWORD,
+	database : process.env.PGDATABASE,
+	port : process.env.PGPORT
 });
 
 var mysqlFunctions = {
